c7c9e8fac15c509d4aada9fe1e3638982efc8f71
virgil <shirrumoni@gmail.com>
2023-06-11 11:32:14 +0200

    changes into js ajax mapping fields

--- src/main/java/com/mail/anonmail/controllers/SendMailController.java
+++ src/main/java/com/mail/anonmail/controllers/SendMailController.java
@@ -2,13 +2,15 @@ package com.mail.anonmail.controllers;
 
 import com.mail.anonmail.models.MessageModel;
 import com.mail.anonmail.utils.smtp.SmtpSender;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SendMailController {
     @PostMapping(value ="/sendMail", consumes = {"application/json"})
     @ResponseBody
-    public void sendMail(@RequestBody MessageModel message) {
+    public ResponseEntity<MessageModel> sendMail(@RequestBody MessageModel message) {
         (new SmtpSender()).sendMail(message);
+        return ResponseEntity.ok(message);
     }
 }
