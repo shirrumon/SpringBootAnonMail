d6d3fc3d54a6fd16744f78d3fe17f3c88ebdd149
virgil <shirrumoni@gmail.com>
2023-06-11 02:32:24 +0200

    fix model generation

--- src/main/java/com/mail/anonmail/routes/SendMailRoute.java
+++ src/main/java/com/mail/anonmail/routes/SendMailRoute.java
@@ -1,17 +1,24 @@
 package com.mail.anonmail.routes;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mail.anonmail.models.MessageModel;
 import com.mail.anonmail.utils.smtp.SmtpSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Properties;
-
 @Controller
 public class SendMailRoute {
-    @PostMapping("/sendMail")
-    public String sendMail(@ModelAttribute("message") MessageModel message) {
-        (new SmtpSender()).sendMail(message);
+    @PostMapping(value ="/sendMail", consumes = {"application/json"})
+    @ResponseBody
+    public String sendMail(@RequestBody String message) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            MessageModel messageModel = objectMapper.readValue(message, MessageModel.class);
+            (new SmtpSender()).sendMail(messageModel);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return "index";
     }
 }
