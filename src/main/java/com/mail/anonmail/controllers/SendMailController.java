d1d32e5063745d42982f2ce6e99f7f84147a2613
virgil <shirrumoni@gmail.com>
2023-06-11 11:09:40 +0200

    update model parsing by di

--- src/main/java/com/mail/anonmail/controllers/SendMailController.java
+++ src/main/java/com/mail/anonmail/controllers/SendMailController.java
@@ -1,23 +1,14 @@
 package com.mail.anonmail.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mail.anonmail.models.MessageModel;
 import com.mail.anonmail.utils.smtp.SmtpSender;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class SendMailController {
     @PostMapping(value ="/sendMail", consumes = {"application/json"})
     @ResponseBody
-    public void sendMail(@RequestBody String message) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            MessageModel messageModel = objectMapper.readValue(message, MessageModel.class);
-            (new SmtpSender()).sendMail(messageModel);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    public void sendMail(@RequestBody MessageModel message) {
+        (new SmtpSender()).sendMail(message);
     }
 }
